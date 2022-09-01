84e31abadfcf96ace98308591b45506c6ac480be
maxpe3447 <m.pech.pr@gmail.com>
2022-09-01 13:40:06 +0300

    Add project files.

---
+++ KntBot/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KntBot.Controllers
+{
+    [Controller]
+    public class HomeController :Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Message = "message";
+
+            return View();
+            //Response.ContentType = "text/html;charset=utf-8";
+            //await Response.SendFileAsync("testPage.html") ;
+        }
+
+        [HttpPost]
+        public IActionResult Index(string name, int age)
+        {
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "testPage.html");
+            return RedirectToAction(nameof(ShowPage));//$"{name}: {age}";
+        }
+
+        [HttpGet]
+        public async Task ShowPage()
+        {
+            await Response.WriteAsync($"BaseDirectory: {AppDomain.CurrentDomain.BaseDirectory}\n" +
+                                      $"Current dir: {Directory.GetCurrentDirectory()}");
+        }
+
+    }
+}
